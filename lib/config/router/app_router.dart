77491ad490fc95c8202c458eb76278e99dff239e
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-10 12:54:31 -0600

    app router refactor and added funciton to listen and redirect when user logs in

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,33 +1,70 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:starpoint_admin/features/auth/presentation/providers/auth_provider.dart';
 
 import '../../features/auth/auth.dart';
 import '../../features/products/products.dart';
+import 'app_router_notifier.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/splash',
-  routes: [
-    ///* Primera pantalla
-    GoRoute(
-      path: '/splash',
-      builder: (context, state) => const CheckAuthStatusScreen(),
-    ),
+final goRouterProvider = Provider<GoRouter>((ref) {
+  final goRouterNotifier = ref.read(goRouterNotifierProvider);
 
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+  return GoRouter(
+    initialLocation: '/splash',
+    refreshListenable: goRouterNotifier,
+    routes: [
+      ///* Primera pantalla
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
+      ///* Auth Routes
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
 
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+      ///* Product Routes
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
+    redirect: (context, state) {
+      final isGoingTo = state.matchedLocation;
+      final authStatus = goRouterNotifier.authStatus;
+
+      print('GoRouter authStatus: $authStatus, isGoingTo: $isGoingTo');
+
+      //* Checking
+      if (isGoingTo == '/spash' && authStatus == AuthStatus.checking) {
+        return null;
+      }
+
+      //* No autenticado
+      if (authStatus == AuthStatus.notAuthenticated) {
+        if (isGoingTo == '/login' || isGoingTo == '/register') {
+          return null;
+        }
+
+        return '/login';
+      }
+
+      //* Autenticado
+      if (authStatus == AuthStatus.authenticated) {
+        if (isGoingTo == '/login' ||
+            isGoingTo == '/register' ||
+            isGoingTo == '/splash') {
+          return '/';
+        }
+      }
+
+      return null;
+    },
+  );
+});
