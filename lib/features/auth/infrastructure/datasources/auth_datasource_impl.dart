eefed43e89d1f53dd5f5e86ae52937cc07e603de
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-09 20:23:44 -0600

    added login method implementation, user mapper and basic auth error classes added

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,6 +1,16 @@
-import 'package:starpoint_admin/features/auth/domain/domain.dart';
+import 'package:dio/dio.dart';
+import 'package:starpoint_admin/config/constants/environment.dart';
 
-class AuthDatasourceImpl extends AuthDatasource{
+import 'package:starpoint_admin/features/auth/domain/domain.dart';
+import 'package:starpoint_admin/features/auth/infrastructure/infrastructure.dart';
+
+class AuthDatasourceImpl extends AuthDatasource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.apiUrl,
+    ),
+  );
+
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -8,9 +18,18 @@ class AuthDatasourceImpl extends AuthDatasource{
   }
 
   @override
-  Future<User> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio.post('/auth/login', data: {
+        'email': email,
+        'password': password,
+      });
+
+      final user = UserMapper.userJsonToEntity(response.data);
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
   }
 
   @override
