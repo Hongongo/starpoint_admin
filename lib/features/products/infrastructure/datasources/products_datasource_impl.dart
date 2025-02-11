7a25794622c868eb2fbf03072e059864c87780fd
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-10 19:21:00 -0600

    get products by page method created

--- lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -1,6 +1,22 @@
+import 'package:dio/dio.dart';
+
+import 'package:starpoint_admin/config/config.dart';
 import 'package:starpoint_admin/features/products/domain/domain.dart';
+import 'package:starpoint_admin/features/products/infrastructure/mappers/product_mapper.dart';
 
 class ProductsDatasourceImpl extends ProductsDatasource {
+  late final Dio dio;
+  final String accessToken;
+
+  ProductsDatasourceImpl({
+    required this.accessToken,
+  }) : dio = Dio(
+          BaseOptions(
+            baseUrl: Environment.apiUrl,
+            headers: {'Authorization': 'Bearer $accessToken'},
+          ),
+        );
+
   @override
   Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
     // TODO: implement createUpdateProduct
@@ -14,9 +30,17 @@ class ProductsDatasourceImpl extends ProductsDatasource {
   }
 
   @override
-  Future<List<Product>> getProductsByPage({int limit = 10, int offset = 0}) {
-    // TODO: implement getProductsByPage
-    throw UnimplementedError();
+  Future<List<Product>> getProductsByPage(
+      {int limit = 10, int offset = 0}) async {
+    final response = await dio.get<List>(
+      '/api/products?limit=$limit&offset=$offset',
+    );
+    final List<Product> products = [];
+    for (final product in response.data ?? []){
+      products.add(ProductMapper.jsonToEntity(product));
+    }
+
+    return products;
   }
 
   @override
