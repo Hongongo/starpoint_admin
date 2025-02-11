09a1dc49b78da12337b582c7c2aaceed9cf64c22
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-10 19:01:24 -0600

    added product domain/infrastructure repository and datasoruces

---
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -0,0 +1,27 @@
+import 'package:starpoint_admin/features/products/domain/domain.dart';
+
+class ProductsDatasourceImpl extends ProductsDatasource {
+  @override
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
+    // TODO: implement createUpdateProduct
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Product> getProductById(String id) {
+    // TODO: implement getProductById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Product>> getProductsByPage({int limit = 10, int offset = 0}) {
+    // TODO: implement getProductsByPage
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Product>> searchProductByTerm(String term) {
+    // TODO: implement searchProductByTerm
+    throw UnimplementedError();
+  }
+}
