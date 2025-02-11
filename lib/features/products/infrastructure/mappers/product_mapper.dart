079d01691bbdc8ca252c5b653897a88afa5c5db2
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-10 19:20:47 -0600

    product mapper added

---
+++ lib/features/products/infrastructure/mappers/product_mapper.dart
@@ -0,0 +1,27 @@
+import 'package:starpoint_admin/config/config.dart';
+import 'package:starpoint_admin/features/auth/infrastructure/infrastructure.dart';
+import 'package:starpoint_admin/features/products/domain/domain.dart';
+
+
+
+class ProductMapper {
+  static jsonToEntity( Map<String, dynamic> json ) => Product(
+    id: json['id'], 
+    title: json['title'], 
+    price: double.parse( json['price'].toString() ), 
+    description: json['description'], 
+    slug: json['slug'], 
+    stock: json['stock'], 
+    sizes: List<String>.from( json['sizes'].map( (size) => size )  ), 
+    gender: json['gender'], 
+    tags: List<String>.from( json['tags'].map( (tag) => tag )  ),
+    images: List<String>.from(
+      json['images'].map( 
+        (String image) => image.startsWith('http')
+          ? image
+          : '${ Environment.apiUrl }/files/product/$image',
+      )
+    ), 
+    user: UserMapper.userJsonToEntity( json['user'] )
+  );
+}
